09289f296339ea531143899fc5c1c71ba36b104e
Esteban Sanjurjo <41419364+webma5ter23@users.noreply.github.com>
2022-09-23 00:17:45 -0300

    initial commit

---
+++ notificaciones/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $fcm_server_key = 'AAAAnYxGH40:APA91bGhIc7pQGs_u8l5Jj-HsLzoUO5WXiX-l9tXx5SCuax2V27w9an4pMEu4IhCbgRhk35xdNl4DDmHXZl6zuL2SyesoFSJPqj-VrIUh0UWf2Q7Sa03jv9K0Nt-omgizTs48811bHPY';
+
+    $fcm_topic = 'radio_delirio_topic';
+
+    $onesignal_app_id = '30b12e67-a9b8-4f1f-917b-a6ddf4ba66e1';
+    
+    $onesignal_rest_api_key = 'MmU0NjM1NGEtZGI0YS00YTM3LTllY2EtNTk4ZTMzMGU1NjE1';
+
+?>
